eca4b94ef64eb9ee8db06d7a946a43ba78ccad6d
JC354R <czarna.kopertowska@gmail.com>
2026-09-24 10:38:28 +0200

    widok

--- app/src/main/java/com/example/quizzaplikacja/MainActivity.java
+++ app/src/main/java/com/example/quizzaplikacja/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.quizzaplikacja;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonNastepne;
+    RadioButton radioButtonA, radioButtonB, radioButtonC;
+    RadioGroup radioGroup;
+    TextView textViewTresc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        buttonNastepne = findViewById(R.id.buttonNastepne);
+        radioButtonA = findViewById(R.id.radioButtonA);
+        radioButtonB = findViewById(R.id.radioButtonB);
+        radioButtonC = findViewById(R.id.radioButtonC);
+        textViewTresc = findViewById(R.id.trescPytaniaTxt);
+        radioGroup = findViewById(R.id.radioGroup);
 
     }
 }
