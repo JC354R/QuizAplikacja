e0cad63a84f89d0a255c1f6eda0183bd60e5bc86
JC354R <czarna.kopertowska@gmail.com>
2026-09-24 10:27:08 +0200

    pytania constructor getter setter

--- app/src/main/java/com/example/quizzaplikacja/Pytanie.java
+++ app/src/main/java/com/example/quizzaplikacja/Pytanie.java
@@ -16,4 +16,58 @@ public class Pytanie {
     private String odpowiedzC;
 
     private int poprawna;
+
+    //constructor
+    public Pytanie(String trescPytania, String odpowiedzA, String odpowiedzB, String odpowiedzC, int poprawna) {
+        this.trescPytania = trescPytania;
+        this.odpowiedzA = odpowiedzA;
+        this.odpowiedzB = odpowiedzB;
+        this.odpowiedzC = odpowiedzC;
+        this.poprawna = poprawna;
+    }
+
+    //getter
+
+
+    public String getTrescPytania() {
+        return trescPytania;
+    }
+
+    public String getOdpowiedzA() {
+        return odpowiedzA;
+    }
+
+    public String getOdpowiedzB() {
+        return odpowiedzB;
+    }
+
+    public String getOdpowiedzC() {
+        return odpowiedzC;
+    }
+
+    public int getPoprawna() {
+        return poprawna;
+    }
+
+    //setter
+
+    public void setTrescPytania(String trescPytania) {
+        this.trescPytania = trescPytania;
+    }
+
+    public void setOdpowiedzA(String odpowiedzA) {
+        this.odpowiedzA = odpowiedzA;
+    }
+
+    public void setOdpowiedzB(String odpowiedzB) {
+        this.odpowiedzB = odpowiedzB;
+    }
+
+    public void setOdpowiedzC(String odpowiedzC) {
+        this.odpowiedzC = odpowiedzC;
+    }
+
+    public void setPoprawna(int poprawna) {
+        this.poprawna = poprawna;
+    }
 }
