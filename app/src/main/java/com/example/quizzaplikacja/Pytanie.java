e9472e1d448aa3c511da9b6cb4c3b58e8754e72e
JC354R <czarna.kopertowska@gmail.com>
2026-09-24 10:25:40 +0200

    klasa pytanie json

--- app/src/main/java/com/example/quizzaplikacja/Pytanie.java
+++ app/src/main/java/com/example/quizzaplikacja/Pytanie.java
@@ -1,4 +1,19 @@
 package com.example.quizzaplikacja;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Pytanie {
+    @SerializedName("tresc")
+    private String trescPytania;
+
+    @SerializedName("odpA")
+    private String odpowiedzA;
+
+    @SerializedName("odpB")
+    private String odpowiedzB;
+
+    @SerializedName("odpC")
+    private String odpowiedzC;
+
+    private int poprawna;
 }
